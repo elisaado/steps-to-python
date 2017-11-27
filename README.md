d1f46cee04911e3ce8a0b97d973ee58d668b5201
elisaado <eli5saado@gmail.com>
2017-11-27 10:09:31 +0100

    Bootstrapping and stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # steps-to-python
 Wikihow-ish thing, rewritten version of elisaado/steps-to, but this is in python/flask
+
+### Dependencies
+See `requirements.txt`, install them all with: `pip install -r requirements.txt`
+
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # This file is part of elisaado/steps-to-python, licensed under the BSD 3-Clause License, if you have not got a copy of 
 # that license with this software, you can find it here: 
 # https://raw.githubusercontent.com/elisaado/steps-to-python/master/LICENSE
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
