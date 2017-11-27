d0c976e568b9566e5ca0adee6b329a57e67faac3
elisaado <eli5saado@gmail.com>
2017-11-27 10:01:17 +0100

    Fix typos ("the that")

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # This file is part of elisaado/steps-to-python, licensed under the BSD 3-Clause License, if you have not got a copy of 
-# the that license with the software, you can find it here: 
+# that license with this software, you can find it here: 
 # https://raw.githubusercontent.com/elisaado/steps-to-python/master/LICENSE
